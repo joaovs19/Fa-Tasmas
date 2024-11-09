f6ff3e5b28b4c71d080db09e9f2c67e21836b320
unknown <rafael.zachesky@gmail.com>
2024-11-09 11:25:42 -0300

    added hunt page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { InitialPageComponent } from './pages/initial-page/initial-page.component';
+import { CacaPageComponent } from './pages/caca-page/caca-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: 'home', component: InitialPageComponent}
+  { path: 'home', component: InitialPageComponent},
+  {path:'caca', component:CacaPageComponent}
 ];
---
+++ src/app/pages/caca-page/caca-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-caca-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './caca-page.component.html',
+  styleUrl: './caca-page.component.scss'
+})
+export class CacaPageComponent {
+
+}
